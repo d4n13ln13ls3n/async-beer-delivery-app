a7cedf1b179a307963aa0515fd2e26180a1433a7
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-30 16:42:00 -0300

    fix: Correções de lint parte 2

--- back-end/src/helpers/Token.js
+++ back-end/src/helpers/Token.js
@@ -1,22 +1,23 @@
-require("dotenv/config");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const jwtOptions = {
+  algorithm: 'HS256',
+  expiresIn: '1d',
+};
+
+const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf-8');
 
 class Token {
-  static jwtOptions = {
-    algorithm: "HS256",
-    expiresIn: "1d",
-  };
-  static jwtSecret = fs.readFileSync("jwt.evaluation.key", "utf-8");
-
   static create(payload) {
-    const token = jwt.sign(payload, Token.jwtSecret, Token.jwtOptions);
+    const token = jwt.sign(payload, jwtSecret, jwtOptions);
 
     return token;
   }
 
   static validate(token) {
-    const payload = jwt.verify(token, Token.jwtSecret);
+    const payload = jwt.verify(token, jwtSecret);
 
     return payload;
   }
--- back-end/src/middlewares/errorHandler/HttpErrorHandler.js
+++ back-end/src/middlewares/errorHandler/HttpErrorHandler.js
@@ -1,6 +1,4 @@
 class HttpErrorHandler extends Error {
-  status;
-
   constructor(status, message) {
     super(message);
     this.status = status;
