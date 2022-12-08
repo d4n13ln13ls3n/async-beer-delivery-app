df7f1cda80b94192c8f90e34e2ca4ce190b38cb1
Adriano Costa <adrianofacosta@gmail.com>
2022-12-08 14:57:59 -0300

    :recycle: refactor: refactor component navbar and genericLink

--- front-end/src/components/GenericLink.jsx
+++ front-end/src/components/GenericLink.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default function GenericLink({ route, name }) {
+export default function GenericLink({ route, name, testid }) {
   return (
-    <Link to={ route }>{ name }</Link>
+    <Link to={ route } data-testid={ testid }>{ name }</Link>
   );
 }
 
 GenericLink.propTypes = {
   route: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  testid: PropTypes.string.isRequired,
 };
