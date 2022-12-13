7f9f0748e199ae3b3043fb5548b75a3623ed00a5
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-12-13 10:35:22 -0300

    fix: arruma nome da tabela sales_products

--- back-end/src/database/migrations/20221129194157-create-salesProducts.js
+++ back-end/src/database/migrations/20221129194157-create-salesProducts.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("salesProducts", {
+    await queryInterface.createTable("sales_products", {
       saleId: {
         type: Sequelize.INTEGER,
         field: "sale_id",
@@ -31,6 +31,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("salesProducts");
+    await queryInterface.dropTable("sales_products");
   },
 };
