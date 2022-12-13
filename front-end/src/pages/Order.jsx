a49131c5db9dc79d6de6995936daedfffdd58f91
Adriano Costa <adrianofacosta@gmail.com>
2022-12-13 16:40:28 -0300

    :sparkles: feat add navbar to order

--- front-end/src/pages/Order.jsx
+++ front-end/src/pages/Order.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import OrderCard from '../components/OrderCard';
 
 export default function Order() {
   return (
-    <div />
+    <>
+      <Navbar />
+      <OrderCard />
+    </>
   );
 }
