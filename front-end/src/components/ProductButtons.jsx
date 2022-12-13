b5e976e1eb216cd162c4c2ea5926b9541b468415
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-12-12 21:21:13 -0300

    feat: concluído decremento de produtos

--- front-end/src/components/ProductButtons.jsx
+++ front-end/src/components/ProductButtons.jsx
@@ -4,37 +4,19 @@ import CartContext from '../context/CartContext';
 
 export default function ProductButtons({ product }) {
   const { cart, setCart } = useContext(CartContext);
-  console.log(cart);
 
-  // const handleDecrease = (item) => {
-  //   const isAlreadyAddedOnCart = cart.some(
-  //     (cartProduct) => cartProduct.id === item.id,
-  //   );
-
-  //   if (isAlreadyAddedOnCart) {
-  //     setCart((prev) => prev.map((cartProduct) => {
-  //       if (cartProduct.id === item.id) {
-  //         return { ...cartProduct, quantity: cartProduct.quantity - 1 };
-  //       }
-
-  //       return cartProduct;
-  //     }));
-  //   } else if (item.quantity === 1) {
-  //     setCart(cart.filter((cartItem) => cartItem.quantity !== 0));
-  //   }
-  // };
-
-  const handleDecrease = () => {
+  function handleDecrease(item) {
     const updatedCart = cart.map((cartItem) => {
-      if (cartItem.quantity <= 0) {
-        return cart.filter((item) => item.quantity <= 0);
+      if (cartItem.id === item.id) {
+        return { ...cartItem, quantity: cartItem.quantity - 1 };
       }
-      cartItem.quantity -= 1;
       return cartItem;
     });
 
-    return setCart(updatedCart);
-  };
+    const filteredCart = updatedCart.filter((cartItem) => cartItem.quantity > 0);
+
+    setCart(filteredCart);
+  }
 
   function handleAddToCart(item) {
     const isAlreadyAddedOnCart = cart.some(
@@ -66,27 +48,12 @@ export default function ProductButtons({ product }) {
     return 0;
   }
 
-  // function updateQuantity(cartProductToUpdate, amount) {
-  //   if (cartProductToUpdate.quantity + amount <= 0) {
-  //     setCart((prev) => prev
-  //       .filter((cartProduct) => cartProduct.id !== cartProductToUpdate.id));
-  //   }
-
-  //   setCart((prev) => prev.map((cartProduct) => {
-  //     if (cartProduct.id === cartProductToUpdate.id) {
-  //       return { ...cartProduct, quantity: cartProduct.quantity + amount };
-  //     }
-
-  //     return cartProduct;
-  //   }));
-  // }
-
   return (
     <div>
       <button
         data-testid={ `customer_products__button-card-rm-item-${product.id}` }
         type="button"
-        onClick={ handleDecrease }
+        onClick={ () => handleDecrease(product) }
       >
         -
       </button>
